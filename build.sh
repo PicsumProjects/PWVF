d663fcccec72fa470adbd2bf21bd1dbb47b5c75b
Codezigineer <53050996+Codezigineer@users.noreply.github.com>
2023-02-27 17:11:55 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 clang -O3 -shared pwvf.c -I./ -o PWVF.so
-clang -O3 -shared --target=wasm32 pwvf.c -I./ -o PWVF.so
+curl https://github.com/PicsumProjects/wclibc/releases/download/latest/wclibc-wasm32.wasm -o libc.wasm
+clang -O3 -shared --target=wasm32 -nostdlib pwvf.c libc.wasm -I./ -o PWVF.so
